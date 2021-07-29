382d41b0c1d45cb25d726e6fa69ab454ca507ce0
Phil <philjacks.1987@gmail.com>
2021-07-29 23:19:17 +0100

    minor fixes

--- js/index.js
+++ js/index.js
@@ -6,43 +6,6 @@ const flkty = new Flickity(carousel, {
 
 })
 
-// // Reduce carousel width for responsiveness
-// const media1 = window.matchMedia(`(max-width: 1550px)`);
-// const media2 = window.matchMedia(`(max-width: 600px)`);
-// const media3 = window.matchMedia(`(min-width: 1550px)`);
-
-
-// const reduceCarouselSize1 = (media) => {
-//   if (media.matches) {
-//     carousel.style.width = `95%`;
-//   } else {
-//     carousel.style.width = `90%`;
-//   }
-// };
-
-// const reduceCarouselSize2 = (media) => {
-//   if (media.matches) {
-//     carousel.style.width = `99%`;
-//   } else {
-//     carousel.style.width = `90%`;
-//   }
-// };
-
-// const increaseCarouselSize = (media) => {
-//   if (media.matches) {
-//     carousel.style.width = `75%`;
-//   }
-// };
-
-// media1.addListener(reduceCarouselSize1);
-// media2.addListener(reduceCarouselSize2);
-// media3.addListener(increaseCarouselSize);
-
-
-// reduceCarouselSize1(media1);
-// reduceCarouselSize2(media2);
-// increaseCarouselSize(media3);
-
 
 // Mailing List Authentication 
 const mailListEmail = document.querySelector('#mail-list-email');

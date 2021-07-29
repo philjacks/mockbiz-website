cc6e2f64cbc4a749d7baaf752c0900fddbc2d7d2
Phil <philjacks.1987@gmail.com>
2021-07-29 22:57:50 +0100

    minor fixes

--- js/index.js
+++ js/index.js
@@ -6,42 +6,42 @@ const flkty = new Flickity(carousel, {
 
 })
 
-// Reduce carousel width for responsiveness
-const media1 = window.matchMedia(`(max-width: 1550px)`);
-const media2 = window.matchMedia(`(max-width: 600px)`);
-const media3 = window.matchMedia(`(min-width: 1550px)`);
+// // Reduce carousel width for responsiveness
+// const media1 = window.matchMedia(`(max-width: 1550px)`);
+// const media2 = window.matchMedia(`(max-width: 600px)`);
+// const media3 = window.matchMedia(`(min-width: 1550px)`);
 
 
-const reduceCarouselSize1 = (media) => {
-  if (media.matches) {
-    carousel.style.width = `95%`;
-  } else {
-    carousel.style.width = `90%`;
-  }
-};
+// const reduceCarouselSize1 = (media) => {
+//   if (media.matches) {
+//     carousel.style.width = `95%`;
+//   } else {
+//     carousel.style.width = `90%`;
+//   }
+// };
 
-const reduceCarouselSize2 = (media) => {
-  if (media.matches) {
-    carousel.style.width = `99%`;
-  } else {
-    carousel.style.width = `90%`;
-  }
-};
+// const reduceCarouselSize2 = (media) => {
+//   if (media.matches) {
+//     carousel.style.width = `99%`;
+//   } else {
+//     carousel.style.width = `90%`;
+//   }
+// };
 
-const increaseCarouselSize = (media) => {
-  if (media.matches) {
-    carousel.style.width = `75%`;
-  }
-};
+// const increaseCarouselSize = (media) => {
+//   if (media.matches) {
+//     carousel.style.width = `75%`;
+//   }
+// };
 
-media1.addListener(reduceCarouselSize1);
-media2.addListener(reduceCarouselSize2);
-media3.addListener(increaseCarouselSize);
+// media1.addListener(reduceCarouselSize1);
+// media2.addListener(reduceCarouselSize2);
+// media3.addListener(increaseCarouselSize);
 
 
-reduceCarouselSize1(media1);
-reduceCarouselSize2(media2);
-increaseCarouselSize(media3);
+// reduceCarouselSize1(media1);
+// reduceCarouselSize2(media2);
+// increaseCarouselSize(media3);
 
 
 // Mailing List Authentication 
